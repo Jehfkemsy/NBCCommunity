99fd627d2b83457a97ab8075c248731370036d80
Fernando <hi@fernandoboza.com>
2018-11-03 18:47:54 -0400

    updated navbar auth & style

--- frontend/src/components/Navbar/NavbarC.js
+++ frontend/src/components/Navbar/NavbarC.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
-import AuthA from "./../../store/action/AuthA";
+import React, { Component } from 'react';
+import AuthA from './../../store/action/AuthA';
+import PropTypes from 'prop-types';
 
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 
 class NavbarC extends Component {
   logout = () => {
@@ -9,19 +10,34 @@ class NavbarC extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <nav className="navbar navbar-light bg-light justify-content-between">
-          <a href="/" className="navbar-brand">CriticizeKing</a>
-          <form className="form-inline">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </nav>
-      </div>
-    )
+    const { auth } = this.props.auth;
+
+    const authLinks = (
+      <nav className="navbar navbar-light bg-light justify-content-between">
+        <a href="/" className="navbar-brand">
+          CriticizeKing
+        </a>
+        <form className="form-inline">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </nav>
+    );
+
+    return <div>{auth ? authLinks : ''}</div>;
   }
 }
+
+NavbarC.propTypes = {
+  auth: PropTypes.bool.isRequired
+};
 
 const mapStateToProps = state => {
   return {

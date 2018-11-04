48222b2b6ab6916fdf0d9057b1fa03896bb94659
mauricio s <msant323@fiu.edu>
2018-11-04 08:39:21 -0500

    material modal wip

--- frontend/src/components/ShowCard/Modal.js
+++ frontend/src/components/ShowCard/Modal.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ResponseA from './../../store/action/ResponseA';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter,InputGroup, InputGroupAddon, Input  } from 'reactstrap';
-<<<<<<< HEAD
-import Comments from '../ResponseCard/ResponseCard';
-import './Modal.css';
-import ResizeReact from 'react-resize-image';
-=======
 import ResponseCard from '../ResponseCard/ResponseCard';
 import { connect } from 'react-redux';
->>>>>>> master
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class showModal extends React.Component {
   constructor(props) {
@@ -58,12 +53,8 @@ class showModal extends React.Component {
   render() {
     return (
       <div>
-        <Button color="danger" onClick={this.toggle}>
-          {this.props.buttonLabel}
-        </Button>
-        <Modal size="lg" isOpen={this.state.modal} toggle={this.toggle} className="show_content">
-          <ModalHeader toggle={this.toggle}>{this.props.buttonLabel}</ModalHeader>
-          <ModalBody>
+        <DialogTitle style={{ marginLeft: "-10px" }}>{this.props.buttonLabel}</DialogTitle>
+
             <div className="row">
               <div className="col-md-6">
                 <img className="img-fluid " alt="Responsive image" src={this.props.image} alt="" />
@@ -80,32 +71,23 @@ class showModal extends React.Component {
                   )
                 })}
               </div>
-            </div>
-                
-          </ModalBody>          
-          <ModalFooter>
+            </div>      
           <div>
-                      <InputGroup>
+            <InputGroup>
               <InputGroupAddon addonType="prepend">@</InputGroupAddon>
-<<<<<<< HEAD
-              <Input placeholder="username " />
-=======
-              <Input
-                type="textarea"                
-                placeholder="Share your thought ..." 
-                value={this.state.inputValue} 
-                onChange={evt => this.handleCommentOnChange(evt)} />
->>>>>>> master
+                <Input
+                  type="textarea"                
+                  placeholder="Share your thought ..." 
+                  value={this.state.inputValue} 
+                  onChange={evt => this.handleCommentOnChange(evt)} />
             </InputGroup>
-                </div>
+          </div>
             <Button color="primary" onClick={this.handlePostResponse}>
               Comment
             </Button>
-            <Button color="secondary" onClick={this.toggle}>
+            <Button color="secondary" onClick={this.props.handleCloseDialog}>
               Go Back
             </Button>
-          </ModalFooter>
-        </Modal>
       </div>
     );
   }
--- frontend/src/components/ShowCard/ShowCard.js
+++ frontend/src/components/ShowCard/ShowCard.js
@@ -3,22 +3,62 @@ Full cards to display the shows
     * on mouse peak, shows title, and description 
     * on click onpen show page 
 */
-import React from 'react';
+import React, {Component} from 'react';
 import { CardImg } from 'reactstrap';
 import PropTypes from 'prop-types';
-import ShowModal from './Modal';
+import Modal from './Modal';
 import Card from '@material-ui/core/Card';
+import CloseIcon from '@material-ui/icons/Close';
+import Dialog from '@material-ui/core/Dialog';
+import CardActionArea from "@material-ui/core/CardActionArea"
+
+export default class MediaCard extends Component {
+  state = {
+    modalOpenState: false,
+  }
+
+  handleModalOpen = () => {
+    this.setState({ modalOpenState: true });
+  };
+
+  handleModalClose = () => {
+    this.setState({ modalOpenState: false });
+  };
+
+  render() {
+    return (
+    <div>
+      <Card style={cardStyle}>
+        <CardActionArea onClick={this.handleModalOpen}>
+          <CardImg height='100%' width='100%' src={this.props.image}/>
+        </CardActionArea>
+      </Card>
 
 
+      <Dialog
+        open={this.state.modalOpenState}
+        onClose={this.handleModalClose}
+        aria-labelledby="form-dialog-title"
+      >
 
-const MediaCard = ({ image, title, description, responses }) => {
-  return (
-    <Card style={cardStyle}>
-      <ShowModal responses={responses} buttonLabel={title} image={image} userName="test" />
-      <CardImg height='100%' width='100%' src={image} />
-    </Card>
-  );
-};
+      <div onClick={this.handleModalClose}>
+        <CloseIcon />
+      </div>
+
+      <Modal
+        responses={this.props.responses} 
+        buttonLabel={this.props.title} 
+        image={this.props.image} 
+        userName="test"
+        openDialog={this.handleModalOpen}
+        handleCloseDialog={this.handleModalClose}
+      />
+
+      </Dialog>
+    </div>
+    );
+  }
+}
 
 MediaCard.propTypes = {
   image: PropTypes.string,
@@ -35,5 +75,3 @@ const cardStyle = {
   boxShadow: '1px 3px 1px',
   borderRadius: 10
 };
-
-export default MediaCard;

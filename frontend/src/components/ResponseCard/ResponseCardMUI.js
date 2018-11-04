812ea114b3445f7e5688cb373d1203d1611422eb
Fernando <hi@fernandoboza.com>
2018-11-04 10:52:58 -0500

    working on modal show

--- frontend/src/components/ResponseCard/ResponseCardMUI.js
+++ frontend/src/components/ResponseCard/ResponseCardMUI.js
@@ -1,48 +1,45 @@
-import React from 'react'
-import Card from "@material-ui/core/Card";
+import React from 'react';
+import Card from '@material-ui/core/Card';
 import { connect } from 'react-redux';
-import ResponseA from './../../store/action/ResponseA'
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import ThumbUp from "@material-ui/icons/ThumbUp";
-import ThumbDown from "@material-ui/icons/ThumbDown";
+import ResponseA from './../../store/action/ResponseA';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import ThumbUp from '@material-ui/icons/ThumbUp';
+import ThumbDown from '@material-ui/icons/ThumbDown';
 import IconButton from '@material-ui/core/IconButton';
 
-
-class ResponseCard extends React.Component { 
-    render() {   
-        return (
-            <div>
-                <Card>
-                    <CardActionArea>
-                    <CardContent>
-                        <Typography gutterBottom variant="headline" component="h2">
-                            {this.props.comments}
-                        </Typography>
-                    </CardContent>    
-                    <IconButton onClick={this.increaseRating} color="primary" aria-label="Add to shopping cart">
-                        <ThumbUp />
-                    </IconButton>      
-                    <IconButton onClick={this.decreaseRating} color="primary" aria-label="Add to shopping cart">
-                        <ThumbDown />
-                    </IconButton>               
-                                     
-                    </CardActionArea>
-                </Card>
-            </div>
-        )
-    }
-    
+class ResponseCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <Card className="show_card_modal">
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="headline" component="h2">
+                {this.props.comments}
+              </Typography>
+            </CardContent>
+            <IconButton onClick={this.increaseRating} color="primary" aria-label="Add to shopping cart">
+              <ThumbUp />
+            </IconButton>
+            <IconButton onClick={this.decreaseRating} color="primary" aria-label="Add to shopping cart">
+              <ThumbDown />
+            </IconButton>
+          </CardActionArea>
+        </Card>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-      responseFn: ResponseA(dispatch)
-    };
+  return {
+    responseFn: ResponseA(dispatch)
   };
-  
-  export default connect(
-    null,
-    mapDispatchToProps
-  )(ResponseCard);
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ResponseCard);

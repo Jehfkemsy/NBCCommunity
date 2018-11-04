877fb786495d2d18310c247fd69b4e5f0ca7081d
Fernando <hi@fernandoboza.com>
2018-11-04 10:21:33 -0500

    carousel css fixed

--- frontend/src/components/ShowCard/ShowCard.js
+++ frontend/src/components/ShowCard/ShowCard.js
@@ -3,19 +3,19 @@ Full cards to display the shows
     * on mouse peak, shows title, and description 
     * on click onpen show page 
 */
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { CardImg } from 'reactstrap';
 import PropTypes from 'prop-types';
 import Modal from './Modal';
 import Card from '@material-ui/core/Card';
 import CloseIcon from '@material-ui/icons/Close';
 import Dialog from '@material-ui/core/Dialog';
-import CardActionArea from "@material-ui/core/CardActionArea"
+import CardActionArea from '@material-ui/core/CardActionArea';
 
 export default class MediaCard extends Component {
   state = {
-    modalOpenState: false,
-  }
+    modalOpenState: false
+  };
 
   handleModalOpen = () => {
     this.setState({ modalOpenState: true });
@@ -27,35 +27,32 @@ export default class MediaCard extends Component {
 
   render() {
     return (
-    <div>
-      <Card style={cardStyle}>
-        <CardActionArea onClick={this.handleModalOpen}>
-          <CardImg height='100%' width='100%' src={this.props.image}/>
-        </CardActionArea>
-      </Card>
+      <div>
+        <Card style={cardStyle}>
+          <CardActionArea onClick={this.handleModalOpen}>
+            <CardImg height="100%" width="100%" src={this.props.image} />
+          </CardActionArea>
+        </Card>
 
+        <Dialog
+          open={this.state.modalOpenState}
+          onClose={this.handleModalClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <div onClick={this.handleModalClose}>
+            <CloseIcon />
+          </div>
 
-      <Dialog
-        open={this.state.modalOpenState}
-        onClose={this.handleModalClose}
-        aria-labelledby="form-dialog-title"
-      >
-
-      <div onClick={this.handleModalClose}>
-        <CloseIcon />
+          <Modal
+            responses={this.props.responses}
+            buttonLabel={this.props.title}
+            image={this.props.image}
+            userName="test"
+            openDialog={this.handleModalOpen}
+            handleCloseDialog={this.handleModalClose}
+          />
+        </Dialog>
       </div>
-
-      <Modal
-        responses={this.props.responses} 
-        buttonLabel={this.props.title} 
-        image={this.props.image} 
-        userName="test"
-        openDialog={this.handleModalOpen}
-        handleCloseDialog={this.handleModalClose}
-      />
-
-      </Dialog>
-    </div>
     );
   }
 }
@@ -70,8 +67,7 @@ const cardStyle = {
   marginTop: 10,
   marginLeft: 30,
   marginBottom: 50,
-  height: '100%',
-  width: '27%',
+
   boxShadow: '1px 3px 1px',
   borderRadius: 10
 };
--- frontend/src/pages/HomePage/Carousel.js
+++ frontend/src/pages/HomePage/Carousel.js
@@ -8,87 +8,103 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 class Caro extends Component {
   state = {
-    contents: [[{title: "",
-                image:"",
-                description: ""}],
-                [{title: "",
-                image:"",
-                description: ""}],
-                [{title: "",
-                image:"",
-                description: ""}]
-              ],
+    contents: [
+      [
+        {
+          title: '',
+          image: '',
+          description: ''
+        }
+      ],
+      [
+        {
+          title: '',
+          image: '',
+          description: ''
+        }
+      ],
+      [
+        {
+          title: '',
+          image: '',
+          description: ''
+        }
+      ]
+    ],
     telemundoLoaded: false,
     syfyLoaded: false,
     usaLoaded: false
-  }
+  };
 
   componentDidUpdate(prevProp) {
-    if(prevProp.telemundoShows !== this.props.telemundoShows)
-    {
-      let telemundoReform = this.remap(this.props.telemundoShows)
-      let syfyReform = this.remap(this.props.syfyShows)
-      let usaReform = this.remap(this.props.usaShows)
-      let tmp = [telemundoReform, syfyReform, usaReform]
-      this.setState({contents: tmp})
-      this.setState({telemundoLoaded: true})
-    }   
-    
-    if(prevProp.syfyShows !== this.props.syfyShows)
-    {
-      let telemundoReform = this.remap(this.props.telemundoShows)
-      let syfyReform = this.remap(this.props.syfyShows)
-      let usaReform = this.remap(this.props.usaShows)
-      let tmp = [telemundoReform, syfyReform, usaReform]
-      this.setState({contents: tmp})
-      this.setState({syfyLoaded: true})
-    }    
-    if(prevProp.usaShows !== this.props.usaShows)
-    {
-      let telemundoReform = this.remap(this.props.telemundoShows)
-      let syfyReform = this.remap(this.props.syfyShows)
-      let usaReform = this.remap(this.props.usaShows)
-      let tmp = [telemundoReform, syfyReform, usaReform]
-      this.setState({contents: tmp})
-      this.setState({usaLoaded: true})
-    } 
+    if (prevProp.telemundoShows !== this.props.telemundoShows) {
+      let telemundoReform = this.remap(this.props.telemundoShows, 'Telemundo');
+      let syfyReform = this.remap(this.props.syfyShows, 'SYFY');
+      let usaReform = this.remap(this.props.usaShows, 'USA');
+      let tmp = [telemundoReform, syfyReform, usaReform];
+      this.setState({ contents: tmp });
+      this.setState({ telemundoLoaded: true });
+    }
+
+    if (prevProp.syfyShows !== this.props.syfyShows) {
+      let telemundoReform = this.remap(this.props.telemundoShows, 'Telemundo');
+      let syfyReform = this.remap(this.props.syfyShows, 'SYFY');
+      let usaReform = this.remap(this.props.usaShows, 'USA');
+      let tmp = [telemundoReform, syfyReform, usaReform];
+      this.setState({ contents: tmp });
+      this.setState({ syfyLoaded: true });
+    }
+    if (prevProp.usaShows !== this.props.usaShows) {
+      let telemundoReform = this.remap(this.props.telemundoShows, 'Telemundo');
+      let syfyReform = this.remap(this.props.syfyShows, 'SYFY');
+      let usaReform = this.remap(this.props.usaShows, 'USA');
+      let tmp = [telemundoReform, syfyReform, usaReform];
+      this.setState({ contents: tmp });
+      this.setState({ usaLoaded: true });
+    }
   }
 
-  remap = (src) => {
+  remap = src => {
     return src.map(show => {
       return {
         title: show.attributes.name,
         image: show.relationships.media.data[0].relationships.image.data.attributes.path,
         description: show.attributes.description
-      }});
-  }
-
+      };
+    });
+  };
 
   render() {
+    console.log(this.state.contents);
     return (
       <div>
-      {this.state.telemundoLoaded && this.state.syfyLoaded && this.state.usaLoaded ? this.state.contents.map(channel => {
-        return(
-          <div className="scrolling-wrapper">
-          <h1>{channel[0].description}</h1>
-            {channel.map(show => {
-              let responses = [];
+        {this.state.telemundoLoaded && this.state.syfyLoaded && this.state.usaLoaded
+          ? this.state.contents.map(channel => {
+              return (
+                <div className="scrolling-wrapper">
+                  <h1>{channel[0].channel}</h1>
+                  {channel.map(show => {
+                    let responses = [];
 
-              this.props.responses.map(response => {
-                if(response.show === show.title)
-                  responses.push(response)
-              })
+                    this.props.responses.map(response => {
+                      if (response.show === show.title) responses.push(response);
+                    });
 
-              return(
-                <ShowCard key={show.image} responses = {responses} image = {show.image} title={show.title} description={show.description}></ShowCard>
-              )
-            })}
-          </div>
-            )
-          }
-        )
-        : console.log("Empty channel list")
-      }
+                    return (
+                      <ShowCard
+                        className="card"
+                        key={show.image}
+                        responses={responses}
+                        image={show.image}
+                        title={show.title}
+                        description={show.description}
+                      />
+                    );
+                  })}
+                </div>
+              );
+            })
+          : console.log('Empty channel list')}
       </div>
     );
   }
